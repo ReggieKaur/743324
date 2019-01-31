af62b749a65909fd3cd287baa2d75562d18ca894
visualstudio <visualstudio@230-PC17>
2019-01-31 11:45:11 -0500

    I created a function with body onload event

--- 743324/js/main.js
+++ 743324/js/main.js
@@ -1,1 +1,12 @@
 ﻿// Your code here!
+
+var date = new Date();
+
+
+function myFunction() {
+
+  
+    document.body.innerHTML = "<h1> The date today is " + (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getYear() + "<h1>"
+}
+
+
